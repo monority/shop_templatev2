2d9c98b5a2d342a09489b5aaa19192f76a3e3f80
monority <chenu.pro@gmail.com>
2025-07-21 20:50:22 +0200

    :lipstick: implement favorites display logic in Favorites component

--- src/display/layout/pages/user/Favorites.jsx
+++ src/display/layout/pages/user/Favorites.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
-
+import { useStore } from '../../../../cfg/state/Store';
 const Favorites = () => {
+	const state_favorites = useStore(state => state.user.favorites);
+	const showFavorites = () => {
+		if (state_favorites && state_favorites.length > 0) {
+			return state_favorites.map((favorite, index) => (
+				<div key={index} className="favorite-item">
+					<h2>{favorite.name}</h2>
+					<p>{favorite.description}</p>
+				</div>
+			));
+		} else {
+			return <p>No favorites found.</p>;
+		}
+	}
 	return (
 		<>
 			<section id="favorites">
@@ -13,6 +26,7 @@ const Favorites = () => {
 							<p>Here you can find all your favorite products.</p>
 						</div>
 					</div>
+					{showFavorites()}
 
 				</div>
 			</section>
