2ebf19e9d8f49b5ce5adc13ffadf1df0760ff8c5
monority <chenu.pro@gmail.com>
2025-07-22 20:13:29 +0200

    :lipstick: update: enhance Favorites component layout and styling with image display

--- src/display/layout/pages/user/Favorites.jsx
+++ src/display/layout/pages/user/Favorites.jsx
@@ -3,11 +3,18 @@ import { useStore } from '../../../../cfg/state/Store';
 const Favorites = () => {
 	const state_favorites = useStore(state => state.user.favorites);
 	const showFavorites = () => {
+		console.log(state_favorites);
 		if (state_favorites && state_favorites.length > 0) {
 			return state_favorites.map((favorite, index) => (
 				<div key={index} className="favorite-item">
-					<h2>{favorite.name}</h2>
-					<p>{favorite.description}</p>
+					<div className="element">
+						<img src={favorite.image} alt={favorite.name} className="favorite-image" />
+					</div>
+					<div className="element">
+						<h2>{favorite.name}</h2>
+						<p>{favorite.description}</p>
+						<p className="price">${parseFloat(favorite.price).toFixed(2)}</p>
+					</div>
 				</div>
 			));
 		} else {
