480c66ef14fa857f7fb45d18f5d360fe68ac1abd
monority <chenu.pro@gmail.com>
2025-07-24 16:50:01 +0200

    :art: update: refactor Cart component to display products and remove favorites functionality

--- src/display/layout/pages/payment/Cart.jsx
+++ src/display/layout/pages/payment/Cart.jsx
@@ -1,16 +1,42 @@
 import React from 'react'
+import { useStore } from '../../../../cfg/state/Store';
 
 const Cart = () => {
-	return (
-		<>
-			<section id="cart">
-				<div className="lyt_container">
-					<div className="wrapper">
+	const state_products = useStore(state => state.user.products);
+	const showCart = () => {
 
+		if (state_products && state_products.length > 0) {
+			return state_products.map((product, index) => (
+				<div key={index} className="favorite-item">
+					<div className="element">
+						<img src={product.image} alt={product.name} className="favorite-image" />
+					</div>
+					<div className="element">
+						<h2>{product.name}</h2>
+						<p>{product.description}</p>
+						<p className="price">${parseFloat(product.price).toFixed(2)}</p>
+						<button onClick={() => removeFavorite(index)} className="btn btn-danger">Remove from Favorites</button>
 					</div>
 				</div>
-			</section>
+			));
+		} else {
+			return <p>No products found.</p>;
+		}
+	}
 
+	return (
+		<>
+			<section id="products">
+				<div className="lyt_container gap4">
+					<div className="wrapper">
+						<div className="element">
+							<h1>Products</h1>
+						</div>
+					</div>
+					{showCart()}
+
+				</div>
+			</section>
 		</>
 	)
 }
